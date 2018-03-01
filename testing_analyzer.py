0b15f93ac06587cadd7da96297edee49c7fb6e26
TimothyChia <timrc96@gmail.com>
2018-03-01 00:06:20 -0600

    Test results for design doc

--- testing_analyzer.py
+++ testing_analyzer.py
@@ -8,8 +8,8 @@ with open('testing_analysis.csv', 'w', newline='') as csvfile:
                             quotechar=',', quoting=csv.QUOTE_MINIMAL)
     analysis_writer.writerow(['threshold','false_positives','false_negatives','true_positives','true_negatives'])
     
-    for step in range(110):
-        threshold = step/100 
+    for step in range(550):
+        threshold = step/500 
         false_positives = 0
         false_negatives = 0
         true_positives = 0
@@ -17,13 +17,13 @@ with open('testing_analysis.csv', 'w', newline='') as csvfile:
         with open('testing_results.csv', newline='') as csvfile:
             results_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
             for row in results_reader:
-                if( float( row[4]) >= threshold ):
+                if( float( row[4]) >= threshold ): # confidence >= threshold
                     if(row[1] == row[3]):
                         true_positives += 1
                     else:
                         false_positives += 1
-                else:
-                    if(row[4] != row[3]):
+                else:                              # confidence < threshold
+                    if(row[1] != row[3]):          # API's best guess != true label
                         true_negatives += 1
                     else:
                         false_negatives +=1
